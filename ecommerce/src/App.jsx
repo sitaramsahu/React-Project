c1f2c1abf0d0f052911b0cce4ee379d69b3d21bd
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-01-20 18:31:55 +0530

    error solved vite @ issue

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button"
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
