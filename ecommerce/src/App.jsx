57791ba609c4a6df1793dc2261c07f5758619034
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-01-20 22:27:10 +0530

    dialog component added

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from "react"
- 
+import { Copy } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -9,6 +9,16 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
@@ -28,6 +38,43 @@ function App() {
         <Button className="bg-transparent text-red-400">Instagram</Button>
         <Button className="bg-transparent text-blue-500">Facebook</Button>
         <Button className="bg-transparent text-red-700">YouTube</Button>
+
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Share</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-md">
+        <DialogHeader>
+          <DialogTitle>Share link</DialogTitle>
+          <DialogDescription>
+            Anyone who has this link will be able to view this.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex items-center space-x-2">
+          <div className="grid flex-1 gap-2">
+            <Label htmlFor="link" className="sr-only">
+              Link
+            </Label>
+            <Input
+              id="link"
+              defaultValue="https://sitaramsahu.blogspot.com/"
+              readOnly
+            />
+          </div>
+          <Button type="submit" size="sm" className="px-3">
+            <span className="sr-only">Copy</span>
+            <Copy />
+          </Button>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Close
+            </Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
 
       </div>
       <Card className="w-[350px] bg-slate-200 ml-8">
