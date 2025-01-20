c40fae6fd94f1c09b9dfac1d396315c2220d6c21
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-01-20 19:53:06 +0530

    Components Added
    Card, Input, Level and Select component Added

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,15 +1,73 @@
+import * as React from "react"
+ 
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 function App() {
-
   return (
     <>
-    <div>
-      <h1> Sita Ram Sahu </h1>
-      <Button>Click me</Button>
-    </div>
+      <div className="flex space-x-2 ml-6">
+        
+        <Button className="bg-transparent text-gray-800">Home</Button>
+        <Button className="bg-transparent text-red-400">Instagram</Button>
+        <Button className="bg-transparent text-blue-500">Facebook</Button>
+        <Button className="bg-transparent text-red-700">YouTube</Button>
+
+      </div>
+      <Card className="w-[350px] bg-slate-200 ml-8">
+      <CardHeader>
+        <CardTitle>E-Commerce</CardTitle>
+        <CardDescription>Building e-commerce Web Application</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" placeholder="Name of your project" />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Framework</Label>
+              <Select>
+                <SelectTrigger id="framework">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent position="popper">
+                  <SelectItem value="next">Next.js</SelectItem>
+                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                  <SelectItem value="astro">Astro</SelectItem>
+                  <SelectItem value="nuxt">Nuxt.js</SelectItem>                  
+                  <SelectItem value="react">React.js</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
