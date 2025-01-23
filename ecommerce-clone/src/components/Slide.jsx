175fec56a89a22e6bcd8598c685db2ada76b483b
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-01-23 13:42:20 +0530

    Update Slide component

--- ecommerce-clone/src/components/Slide.jsx
+++ ecommerce-clone/src/components/Slide.jsx
@@ -1,36 +1,40 @@
-import * as React from "react"
- 
-import { Card, CardContent } from "@/components/ui/card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+import * as React from "react";
 
+import { Card, CardContent } from "@/components/ui/card";
+import {  Carousel,  CarouselContent,} from "@/components/ui/carousel";
+import {CarouselItem,  CarouselNext,  CarouselPrevious,} from '@/components/ui/carousel'
 const Slide = () => {
   return (
     <div>
-      <Carousel className="w-full max-w-xs">
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-5">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+      <Carousel
+        className="w-full "
+        opts={{
+            align: "start",
+            loop: true,
+        }}
+        w-full
+        max-w-sm
+      >
+        <div className="items-center justify-center ">
+        <CarouselContent className="">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <CarouselItem key={index} className=" basis-1/3">
+              <div className="p-2 ">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        </div>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Slide
+export default Slide;
