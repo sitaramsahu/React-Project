ee3ad5ecfddb8ddaa1a255ffea1abcaf5b9c9e7d
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-02-11 13:37:52 +0530

    Update Home.jsx

--- ecommerce/src/Pages/Home.jsx
+++ ecommerce/src/Pages/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className="min-h-screen">
       <h1 className="text-2xl font-bold">Home Page</h1>
-      <div className="p-5 ">
+      {/* <div className="p-5 ">
         <div className="w-full justify-center">
           <img
             src="https://sitaramstationers.shiprocket.co/post_order/img/order_search_banner_1.png"
             alt="Spipment"
           />
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
