36fe1dbf36cec99e2f8a2810808cb8adf93a6c01
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-02-14 11:52:51 +0530

    Update App.tsx

--- typescript-app/src/App.tsx
+++ typescript-app/src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-black h-10"></div>;
+  return <div className="bg-black h-10 text-white"> My Type Scripe Page</div>;
 }
 
 export default App;
