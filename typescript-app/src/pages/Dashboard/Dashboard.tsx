c8731508be11dca3b6553d3754f05f924afac211
Sita Ram Sahu <103084791+sitaramsahu@users.noreply.github.com>
2025-02-18 17:38:17 +0530

    array component

--- typescript-app/src/pages/Dashboard/Dashboard.tsx
+++ typescript-app/src/pages/Dashboard/Dashboard.tsx
@@ -4,74 +4,11 @@ import Layout from "@/components/Sidebar/Layout";
 import Header from "@/components/Header/Header";
 import ProductList from "@/components/Products/ProductList";
 import ProductCard from "@/components/Products/ProductCard";
+import { products } from "@/components/AllProductsList/CardProduct";
 
 const Dashboard: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
-
-  const products = [
-    {
-      title: "GitHub Account",
-      price: 15449.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon-github.jpg",
-    },
-    {
-      title: "YouTube Account",
-      price: 38000.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_yt.png",
-    },
-    {
-      title: "Website",
-      price: 49000.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_web.png",
-    },
-    {
-      title: "Twitter Account",
-      price: 20999.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_twitter.png",
-    },
-    {
-      title: "Instagram Account",
-      price: 14599.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_insta.png",
-    },
-    {
-      title: "Facebook Account",
-      price: 13999.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_facebook.png",
-    },
-    {
-      title: "Gmail Account",
-      price: 98999.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_gmail.png",
-    },
-    {
-      title: "LinkedIn Account",
-      price: 25999.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/icon_linkedin.png",
-    },
-    {
-      title: "Book",
-      price: 499.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/2._SS420.png",
-    },
-    {
-      title: "Book",
-      price: 499.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/1._SS420.png",
-    },
-    {
-      title: "Book",
-      price: 499.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/4._SS420.png",
-    },
-    {
-      title: "Book",
-      price: 499.99,
-      image: "https://asrsnews.in/wp-content/uploads/2025/02/6._SS420.png",
-    },
-  ];
-
   return (
     <div className="flex">
       <Layout sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
